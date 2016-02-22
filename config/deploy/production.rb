7587f80f5d36672c81f1ee499a04d0b20ddb5235
Jade-L <lemaissi.jade@gmail.com>
2016-02-22 15:13:05 +0100

    update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,3 @@
-set :stage, :production
-
-# Replace 127.0.0.1 with your server's IP address!
-server '51.254.222.87', user: 'deploy', roles: %w{web app}
-
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
@@ -19,7 +14,8 @@ role :db,  %w{deploy@vps233847.ovh.net}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '51.254.222.87', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :stage, :production
+server 'vps233847.ovh.net', user: 'deploy', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
